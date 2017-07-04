beaf7e8c204863f6af0eb7bb01e0ac527ccc5945
CANG-PC\Cang <trcangit@gmail.com>
2017-07-05 00:43:49 +0700

    fix model

---
+++ StoreManager.Model/Models/Tag.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace StoreManager.Model.Models
+{
+    [Table("Tags")]
+    public class Tag
+    {
+        [Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Type { get; set; }
+    }
+}
