b16b5d88b76f0918480d5053dc5672424daa7483
CANG-PC\Cang <trcangit@gmail.com>
2017-07-09 10:28:22 +0700

    Model table "OrderDetail,Orders, Post ,PostCategorie,PostTag,ProductTag,Slide"

---
+++ StoreManager.Model/Models/PostTag.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace StoreManager.Model.Models
+{
+    [Table("PostTags")]
+    public class PostTag
+    {
+        [Key]
+        public int PostID { set; get; }
+
+        [Key]
+        [Column(TypeName = "varchar")]
+        [MaxLength(50)]
+        public string TagID { set; get; }
+
+        [ForeignKey("PostID")]
+        public virtual Post Post { set; get; }
+
+        [ForeignKey("TagID")]
+        public virtual Tag Tag { set; get; }
+    }
+}
