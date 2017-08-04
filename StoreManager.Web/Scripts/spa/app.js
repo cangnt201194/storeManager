ef48eff5dd2a20e1c052ad43c0b3e38ef2f351fd
CANG-PC\Cang <trcangit@gmail.com>
2017-08-05 04:12:53 +0700

    What is scope and rootscope? scope in scope .Overiwer scope.

---
+++ StoreManager.Web/Scripts/spa/app.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+var myApp = angular.module("myModule", []);
+//cach 1 khai bao
+myApp.controller("myController", myController);
+myApp.controller("myController1", myController1);
+myApp.controller("schoolController", schoolController);
+
+myController.$inject = ['$scope', '$rootScope'];
+//declare scope long nhau
+function schoolController($scope, $rootScope) {
+    $scope.message = "test from schoolController.";
+}
+function myController($scope)
+{
+     $scope.message = "This is my message from Controller";
+}
+function myController1($scope, $rootScope) {
+   // $scope.message = "This is my message from Controller1";
+}
