800462bcbc5de32b8daf1188899aa441fb220ee6
CANG-PC\Cang <trcangit@gmail.com>
2017-08-09 15:23:16 +0700

    Paging  for list

--- StoreManager.Web/API/ProductCategoryController.cs
+++ StoreManager.Web/API/ProductCategoryController.cs
@@ -1,32 +1,45 @@
 ﻿using AutoMapper;
+using StoreManager.Model.Models;
+using StoreManager.Service;
+using StoreManager.Web.Infrastructure.Core;
+using StoreManager.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using StoreManager.Model.Models;
-using StoreManager.Service;
-using StoreManager.Web.Infrastructure.Core;
-using StoreManager.Web.Models;
 
 namespace StoreManager.Web.API
 {
     [RoutePrefix("api/productcategory")]
     public class ProductCategoryController : ApiControllerBase
     {
-        IProductCategoryService _productCategoryService;
-        public ProductCategoryController(IErrorService errorService,IProductCategoryService productCategoryService) :base(errorService)
+        private IProductCategoryService _productCategoryService;
+
+        public ProductCategoryController(IErrorService errorService, IProductCategoryService productCategoryService) : base(errorService)
         {
             this._productCategoryService = productCategoryService;
         }
+
         [Route("getall")]
-        public HttpResponseMessage GetAll(HttpRequestMessage request)
+        public HttpResponseMessage GetAll(HttpRequestMessage request, int page, int pageSize)
         {
-            return CreateHttpResponse(request, () =>{
-                 var model = _productCategoryService.GetAll();
-                var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
-                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+            return CreateHttpResponse(request, () =>
+            {
+                int totalRow = 0;
+                var model = _productCategoryService.GetAll();
+                totalRow = model.Count();
+                var query = model.OrderByDescending(x => x.CreatedDate).Skip(page * pageSize).Take(pageSize);
+                var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(query);
+                var paginationSet = new PaginationSet<ProductCategoryViewModel>()
+                {
+                    Items = responseData,
+                    Page = page,
+                    TotalCount = totalRow,
+                    TotalPages = (int)Math.Ceiling((decimal)totalRow / pageSize)
+                };
+                var response = request.CreateResponse(HttpStatusCode.OK, paginationSet);
                 return response;
             });
         }
--- StoreManager.Web/app/components/product_categories/productCategoryListController.js
+++ StoreManager.Web/app/components/product_categories/productCategoryListController.js
@@ -3,16 +3,28 @@
     app.controller('productCategoryListController', productCategoryListController);
     productCategoryListController.$inject = ['$scope', 'apiService'];
     function productCategoryListController($scope, apiService) {
-        $scope.productcategories = [];
-        $scope.getProductcategories = getProductCatefories;
+        $scope.productCategories = [];
+        $scope.getProductcategories = getProductCategories;
+        $scope.pageCount = 0;
+        $scope.page = 0;
 
-        function getProductCatefories() {
-            apiService.get('/api/productcategory/getall', null, function (result) {
-                $scope.productcategories = result.data;
+        function getProductCategories(page) {
+            page = page || 0;
+            var config = {
+                params: {
+                    page: page,
+                    pageSize:2
+                }
+            }
+            apiService.get('/api/productcategory/getall', config, function (result) {
+                $scope.productCategories = result.data.Items;
+                $scope.page = result.data.Page;
+                $scope.pagesCount = result.data.TotalPages;
+                $scope.totalCount = result.data.TotalCount;
             }, function () {
                 console.log('Load product category failed')
             });         
         }
-        $scope.getProductcategories();
+        $scope.getProductcategories();      
     }
 })(angular.module('storeManager.product_categories'));
